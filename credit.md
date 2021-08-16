feb185c842490b2eb88d53232f76f7b6d08d78d5
Balaiseee <57873756+Balaiseee@users.noreply.github.com>
2021-08-16 19:46:57 +0200

    Initial commit

---
+++ credit.md
@@ -0,0 +1,4 @@
+Header background Image
+https://unsplash.com/photos/_dJCBtdUu74?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
+Logos
+Iconfinder
---
+++ js/main.js
@@ -0,0 +1,10 @@
+var lazyLoadInstance = new LazyLoad({});
+wow = new WOW({
+  boxClass: "wow",
+  animateClass: "animate__animated",
+  offset: 0,
+  mobile: true,
+  live: true,
+});
+wow.init();
+new WOW().init();
